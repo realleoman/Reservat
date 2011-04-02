ffb0f79c78d8f7a7e6ccdf79be6ee30ec585a7d4
Alfredo Cruz <alfredox@gmail.com>
2011-04-02 16:12:26 -0600

    Creación de los modelos: Asset (Resource) AssetType AssetReservation
    Se configuró la base de datos.
    
    /Alfredo

---
+++ app/models/asset_reservation.rb
@@ -0,0 +1,2 @@
+class AssetReservation < ActiveRecord::Base
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Reservat::Application.routes.draw do
+  resources :assets
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
