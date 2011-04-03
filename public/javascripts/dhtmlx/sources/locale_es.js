eaf86c4dbf906756e67f7cbabe3aa582ee148615
Leobardo Cortes <leocortes@gmail.com>
2011-04-02 21:06:38 -0600

    Updated Dhtmlx

---
+++ public/javascripts/dhtmlx/sources/locale_es.js
@@ -0,0 +1,52 @@
+/*
+	@Autor Manuel Fernandez Panzuela - www.mfernandez.es
+*/
+scheduler.locale={
+	date:{
+		month_full:["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+		month_short:["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
+		day_full:["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
+    	day_short:["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"]
+    },
+    labels:{
+    	dhx_cal_today_button:"Hoy",
+    	day_tab:"Día",
+    	week_tab:"Semana",
+    	month_tab:"Mes",
+    	new_event:"Nuevo evento",
+		icon_save:"Guardar",
+		icon_cancel:"Cancelar",
+		icon_details:"Detalles",
+		icon_edit:"Editar",
+		icon_delete:"Eliminar",
+		confirm_closing:"", //"Sus cambios se perderán, continuar ?"
+		confirm_deleting:"El evento se borrará definitivamente, continuar ?",
+		section_description:"Descripción",
+		section_time:"Periodo de Tiempo",
+		section_location:"Lugar",
+		full_day:"Todo el día",
+		unit_tab:"Unidad",
+		
+		confirm_recurring:"¿Desea modificar el conjunto de eventos repetidos?",
+		section_recurring:"Repita el evento",
+		button_recurring:"Impedido",
+		button_recurring_open:"Permitido",
+		
+		/*agenda view extension*/
+		agenda_tab:"Agenda",
+		date:"Fecha",
+		description:"Descripción",
+		
+		/*map extension*/
+		map_tab:"Mapa", // name of the map tab
+		section_location: "Posición", // name of the location section in lightbox
+		marker_geo_success: "Acá estás.", // message in info window in case of successful geolocation response
+		marker_geo_fail: "Disculpa, no pudimos ubicar tu posición.", // message in info window in case of unsuccessful geolocation response
+	
+	
+		/*year view extension*/
+		year_tab:"Año"
+		
+		
+	}
+}
