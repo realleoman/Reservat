02bc8e7a083e5dca8cf9b8699643e3f4816eb69a
Alfredo Cruz <alfredox@gmail.com>
2011-04-03 14:36:56 -0500

    Se agregó has_conflict? a reservation: se agregó un intento de validación pero falla, así que se le cambió el nombre

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,14 @@
 class Reservation < ActiveRecord::Base
   belongs_to :asset
+
+
+  def has_conflict_with?(start_time, stop_time)
+    (start_time >= start_at && start_time <= stop_at) || (stop_time >= start_at && stop_time <= stop_at)
+  end
+
+  def validate_no_funciona
+    if reservation = asset.reservations.find {|r| r.has_conflict_with?(start_at, stop_at)}
+      errors[:base] << "There was a time conflict with reservation #{reservation.id}"
+    end
+  end
 end
